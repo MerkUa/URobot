268c84616e79795db160a97c4137425a956dfa37
viacheslav.merkulov <viacheslav.merkulov@ormdigital.com>
2020-01-17 18:24:36 +0200

    init

--- ChatBot/app/src/main/java/com/urobot/droid/data/model/BotContentItem.kt
+++ ChatBot/app/src/main/java/com/urobot/droid/data/model/BotContentItem.kt
@@ -4,8 +4,6 @@ data class BotContentItem(
     var id: Int?,
     var parent_id: Int?,
     var name: String?,
-    var action: Int?,
-    var isEmpty: Boolean,
     var description: String,
     var list_buttons: ArrayList<ServiceButtons>?
 )
